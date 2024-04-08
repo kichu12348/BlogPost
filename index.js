2f95fe269d49eac5712e5081d4f6a6196ec2539f
kichu12348 <rmahadevan574@gmail.com>
2024-04-08 22:16:20 +0530

    mess3a4g22e

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const userRoutes = require('./routes/user');
 const staticRoutes = require('./routes/staticRoute');
@@ -6,33 +7,23 @@ const blogRoutes = require('./routes/blog');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
-//
-const {connectDB} = require('./connection');
+// mongodb connection setup
+const { connectDB } = require('./connection');
 connectDB("mongodb+srv://kichu12348:tk288174@cluster0.krglllg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
-//mongodb+srv://kichu12348:tk288174@blogy.nzqcolx.mongodb.net/?retryWrites=true&w=majority
+const port = process.env.PORT || 8000;
 
-
-
-//port
-const port =process.env.PORT || 8000;
-
-//view engine
+// view engine setup
 app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
 
-
-//middlewares
-app.use(express.urlencoded({extended:false}));
+// middlewares
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
-//routes
+// routes
 app.use('/', staticRoutes);
 app.use('/', userRoutes);
 app.use('/blog', blogRoutes);
-
-
-
 
 app.listen(port, () => console.log(`Server is running on port ${port} 😎`));
